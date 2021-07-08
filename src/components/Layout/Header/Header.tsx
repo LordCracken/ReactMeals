c09cadef5f8b9a3bdf515da929b1112ad7faa8d3
LordCracken <vladislav.yakimovskiy@mail.ru>
2021-07-08 19:07:48 +0300

    added dummy HeaderCartButton

--- src/components/Layout/Header/Header.tsx
+++ src/components/Layout/Header/Header.tsx
@@ -1,3 +1,5 @@
+import HeaderCartButton from '../HeaderCartButton/HeaderCartButton';
+
 import classes from './Header.module.css';
 
 import mealsImage from '../../../assets/meals.jpg';
@@ -6,7 +8,7 @@ const Header = () => (
   <>
     <header className={classes.header}>
       <h1>ReactMeals</h1>
-      <button>Cart</button>
+      <HeaderCartButton />
     </header>
     <div className={classes['main-image']}>
       <img src={mealsImage} alt="A table full of delicious food!" />
