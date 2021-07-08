674cb8d09a116f9757fd700a29e656793d7d380a
LordCracken <vladislav.yakimovskiy@mail.ru>
2021-07-08 22:48:58 +0300

    added dynamic cart items amount for HeaderCartButton component

--- src/components/Layout/HeaderCartButton/HeaderCartButton.tsx
+++ src/components/Layout/HeaderCartButton/HeaderCartButton.tsx
@@ -1,3 +1,6 @@
+import { useContext } from 'react';
+import CartContext from '../../../store/cart-context';
+
 import CartIcon from '../../Cart/CartIcon';
 
 import classes from './HeaderCartButton.module.css';
@@ -6,14 +9,20 @@ interface HeaderCartButtonProps {
   onClick: () => void;
 }
 
-const HeaderCartButton = ({ onClick }: HeaderCartButtonProps) => (
-  <button className={classes.button} onClick={onClick}>
-    <span className={classes.icon}>
-      <CartIcon />
-    </span>
-    <span>Your Cart</span>
-    <span className={classes.badge}>3</span>
-  </button>
-);
+const HeaderCartButton = ({ onClick }: HeaderCartButtonProps) => {
+  const cartCtx = useContext(CartContext);
+
+  const numberOfCartItems = cartCtx.items!.reduce((curNumber, item) => curNumber + item.amount, 0);
+
+  return (
+    <button className={classes.button} onClick={onClick}>
+      <span className={classes.icon}>
+        <CartIcon />
+      </span>
+      <span>Your Cart</span>
+      <span className={classes.badge}>{numberOfCartItems}</span>
+    </button>
+  );
+};
 
 export default HeaderCartButton;
